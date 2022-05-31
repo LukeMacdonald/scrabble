f88b69fb80adeff65b06ce151d6c1f2e77c403c0
LukeMacdonald <s3888490@student.rmit.edu.au>
2022-05-31 15:16:05 +1000

    cleaned code

--- scrabble.cpp
+++ scrabble.cpp
@@ -101,8 +101,8 @@ void new_game(){
    // declares Board
    Board* board = new Board();
    // initialises TileBag
-   TileBag* tileBag = new TileBag("ScrabbleTiles");
-   Dictionary* dicationary = new Dictionary("wordlist");
+   TileBag* tileBag = new TileBag("configs/ScrabbleTiles");
+   Dictionary* dicationary = new Dictionary("configs/wordlist");
    board->set_dictionary(dicationary);
    // initialises board
    board->print();
@@ -187,7 +187,7 @@ void game_over(PlayerHand* player1,PlayerHand* player2){
 void save(PlayerHand* p1, PlayerHand* p2, TileBag* bag, Board* board){
    
    std::string file_name = p1->get_filename();
-   std::ofstream output_file(file_name + ".txt");
+   std::ofstream output_file("saved_games/"+file_name + ".txt");
    
    save_scores(p1->get_player_name(),p2->get_player_name(),p1->get_score(),p2->get_score());
    p1->save_details(output_file);
@@ -208,21 +208,35 @@ void load(){
    Board* board = new Board();
    TileBag* bag = new TileBag();
 
+   std::ifstream input_file;
+   std::string player;
+   std::cout << "\nSaved Games:" << std::endl;
+   std::cout <<"------------"<< std::endl;
+   input_file.open("saved_games/game_files.txt");
+   std::string game;
+   int counter = 1;
+   
+   while(getline(input_file,game)){
+      std::cout << counter << "." << game <<std::endl;
+      counter++;
+   }
+   input_file.close();
+
    // get name of saved game file
    std::string fileName;
-   std::cout<<"Enter Game Name:" <<std::endl;
+   std::cout<<"\nEnter Game Name:" <<std::endl;
    std::cin.ignore();
    
    getline(std::cin,fileName);
    // checks game exists
-   while (checkFile(fileName + ".txt") == false) {
+   while (checkFile("saved_games/" + fileName + ".txt") == false) {
       std::cout << "Game Doesn't Exist" << std::endl;
       std::cout<<"Enter Game Name:" << std::endl;
       getline(std::cin,fileName);
    }
 
-   std::ifstream input_file;
-   input_file.open(fileName + ".txt");
+   
+   input_file.open("saved_games/" + fileName + ".txt");
 
    p1->load_details(input_file);
    p2->load_details(input_file);
@@ -271,7 +285,7 @@ void high_scores(){
 void save_scores(std::string p1_name, std::string p2_name,int p1_score,int p2_score){
    std::ifstream input_file;
    std::string player;
-   input_file.open("high_scores.txt");
+   input_file.open("configs/high_scores.txt");
    int score;
    bool p1Found = false;
    bool p2Found = false; 
@@ -305,7 +319,7 @@ void save_scores(std::string p1_name, std::string p2_name,int p1_score,int p2_sc
    }
    sort(all_scores.begin(), all_scores.end(), sortbysec);
 
-   std::ofstream output_file("high_scores.txt");
+   std::ofstream output_file("configs/high_scores.txt");
    for (int i = 0; i < all_scores.size();i++){
       output_file << std::get<0>(all_scores[i]) << " "<< std::get<1>(all_scores[i]);
       if (i < all_scores.size() - 1){
